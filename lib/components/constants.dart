8bff0baadf11c040598c052a8c9dabec2dbe6221
Gabriel Dias da Silva <gdias9487@gmail.com>
2020-12-20 20:42:49 -0300

    Login 2.0

---
+++ lib/components/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+//Paleta de cores PlusFit
+const pdarkred = Color(0xFF26020F);
+const pblack = Color(0xFF140108);
+const plightred = Color(0xFFA5062B);
+const porange = Color(0xFFC12402);
+const pyellow = Color(0xFFC79D0A);
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:plusfit/components/assets.dart';
+import 'package:plusfit/loginpage.dart';
+import 'package:plusfit/components/constants.dart';
 
 void main() {
   runApp(PFApp());
@@ -13,114 +14,8 @@ class PFApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
-      home: MyHomePage(title: 'PlusFit'),
+      home: LoginPage(title: 'PlusFit'),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  stops: [0.5, 0.9],
-                  colors: [Colors.black, Color(0xFF26020F)])),
-          child: ListView(children: <Widget>[
-            SizedBox(
-              height: 30,
-            ),
-            SizedBox(
-                height: 180,
-                width: 180,
-                child: Image.asset("assets/Plusfit_logo.png")),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-              child: Container(
-                height: 310,
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20)),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    SizedBox(
-                      height: 5,
-                    ),
-                    TextFieldContainer(
-                      child: TextField(
-                        style: new TextStyle(color: Colors.black, fontSize: 18),
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(30)),
-                            fillColor: Colors.blue,
-                            prefixIcon: Icon(Icons.account_circle),
-                            labelText: 'E-mail',
-                            labelStyle: TextStyle(color: Colors.black)),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    TextFieldContainer(
-                      child: TextField(
-                        obscureText: true,
-                        style: new TextStyle(color: Colors.black, fontSize: 18),
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(30)),
-                            prefixIcon: Icon(Icons.lock_outline),
-                            suffixIcon: Icon(Icons.visibility_off),
-                            labelText: 'Senha',
-                            labelStyle: TextStyle(color: Colors.black)),
-                      ),
-                    ),
-                    Container(
-                      height: 32,
-                      decoration: BoxDecoration(color: Colors.white),
-                      child: TextButton(
-                        onPressed: () {},
-                        child: Text("Forgot Password?"),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                        child: Column(
-                      children: <Widget>[
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              textStyle: TextStyle(fontSize: 16),
-                              minimumSize: Size(150, 45),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20))),
-                          onPressed: () {},
-                          child: Text("Login"),
-                        ),
-                      ],
-                    ))
-                  ],
-                ),
-              ),
-            ),
-          ])),
     );
   }
 }
