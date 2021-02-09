2cc301c2dc6c2985cbc310fc2cc30ddbd024f3f8
jurupocA <jurupoc222@gmail.com>
2021-02-09 14:43:59 -0300

    Treinos

---
+++ lib/src/exercise/superior/models.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Exercicio{
+  String nome;
+  int intervalo;
+  final repeticao;
+  int series;
+
+  Exercicio({this.nome, this.intervalo, this.repeticao, this.series});
+
+  getNome(){
+    return this.nome;
+  }
+  getIntervalo(){
+    return this.intervalo;    
+  }
+  getRepeticao(){
+    return this.repeticao;
+  }
+  getSeries(){
+    return this.series;
+  }
+
+}
+
+main() {
+
+  var exercicio_1 =
+      new Exercicio(nome: "Flexão", 
+                    intervalo: 30, 
+                    repeticao: 12, 
+                    series: 3
+                  );
+
+  print(exercicio_1.getNome());
+}
